526b539d9645f9c137e7ba477b3e138cc19f3cad
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-27 14:12:02 +0530

    spring example

---
+++ src/main/java/com/spring/personalFinance/FinanceManagementApplication.java
@@ -0,0 +1,10 @@
+package com.spring.personalFinance;
+
+public class FinanceManagementApplication {
+    public class FinanceManagementApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(FinanceManagementApplication.class, args);
+        }
+    }
+
+}
