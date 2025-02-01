4718fe6fc0be3890037c91b665c8f49d86d010fe
Bhaveshpatil45 <bp700259@gmail.com>
2025-02-01 20:39:20 +0530

    annotation

---
+++ spring-annotation-example/src/main/java/com/helloworld/service/HelloService.java
@@ -0,0 +1,24 @@
+package com.helloworld.service;
+
+
+import com.helloworld.model.Address;
+import com.helloworld.repository.HelloRepository;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+@Service
+public class HelloService {
+    @Autowired
+    private HelloRepository helloRepository;
+
+    private String name;
+    private Address address;
+
+    public void sayHello(){
+        helloRepository.sayHello(name,address.getCity().getName());
+    }
+}
