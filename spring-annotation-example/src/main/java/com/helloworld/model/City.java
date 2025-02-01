40eb229e6eac102dc9b5e2b5e30bb60fc7871607
Bhaveshpatil45 <bp700259@gmail.com>
2025-02-01 19:59:48 +0530

    annotation

--- spring-annotation-example/src/main/java/com/helloworld/model/City.java
+++ spring-annotation-example/src/main/java/com/helloworld/model/City.java
@@ -1,4 +1,12 @@
 package com.helloworld.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Builder
+@Getter
 public class City {
+    private String name;
 }
